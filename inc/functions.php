6a03386ffac7cf94cc5625c55767b215ab450f05
Joseph Yhu <josephsyhu@gmail.com>
2022-03-10 00:00:36 -0500

    Change sorting for search list back to score.

--- inc/functions.php
+++ inc/functions.php
@@ -128,7 +128,7 @@ function get_mediaList($type, $page, $search) {
                 currentPage,
                 lastPage,
             },
-            media (type: $type, search: $search, sort: TITLE_ROMAJI) {
+            media (type: $type, search: $search, sort: SCORE_DESC) {
                 title {
                     romaji,
                     english,
