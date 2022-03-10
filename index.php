19626bd8023f365abc583ab62a48468a3ce4ae71
Joseph Yhu <josephsyhu@gmail.com>
2022-03-09 20:29:40 -0500

    Make a minor change.

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ require_once 'inc/header.php';
         echo "<input type='number' id='page' name='page' required><br>";
         echo "<button type='submit'>Search</button><br>";
         if (isset($type) && isset($search)) {
-            echo "<h2 class='media-button btn'>Searched for $search in $type</h2>";
+            echo "<h2>Searched for $search in $type</h2>";
         }
         echo "<div class='media'>";
         try {
