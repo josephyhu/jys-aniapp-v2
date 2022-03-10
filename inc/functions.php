257dd9c7f03c02f794338cacdb7dcfc40c2573cf
Joseph Yhu <josephsyhu@gmail.com>
2022-03-09 20:20:56 -0500

    Fix minor error.

--- inc/functions.php
+++ inc/functions.php
@@ -121,7 +121,7 @@ function get_userMediaList($userId, $type, $status) {
     return $arr['data']['MediaListCollection']['lists'][0]['entries'];
 }
 
-function get_mediaList($type, $page = '', $perPage = '', $search) {
+function get_mediaList($type, $page, $perPage, $search) {
     $query = 'query ($page: Int, $perPage: Int, $type: MediaType, $search: String) {
         Page (page: $page, perPage: $perPage) {
             pageInfo {
