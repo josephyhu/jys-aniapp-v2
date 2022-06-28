aef34dfdd78d120071785273b35323d1854a43b1
Joseph Yhu <josephsyhu@gmail.com>
2022-06-28 15:03:33 -0400

    Testing

--- animelist.php
+++ animelist.php
@@ -77,8 +77,8 @@ require_once 'inc/header.php'; ?>
             echo '<table>';
             echo '<tbody>';
             echo '<tr>';
-            for ($i = 0; $i < count($data['mediaList']); $i++) {
-                if ($data['mediaList'][$i] != '') {
+            for ($i = 1; $i <= count($data['mediaList']); $i++) {
+                if ($data['mediaList'][$i-1] != '') {
                     echo "<td><img src='" . $data['mediaList'][$i]['media']['coverImage']['medium'] . "' alt='cover'></td>";
                 }
                 if ($i % 10 == 0) {
